ca92f7577b151f031808bfaadbe808c455232820
Jordan ( j8 ) <jordan@relayr.de>
2016-07-21 23:43:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Nic.io Domain Scanner
+
+<img width="594" alt="screen shot 2016-07-21 at 23 37 12" src="https://cloud.githubusercontent.com/assets/818400/17039745/2361715e-4f9c-11e6-8757-cb59b07a7984.png">
+
+#Intro
+
+I was looking recently for free .io domains for some of my OSS projects and than I thought it will be nice to have spme commandline utility to scan for domain names by list of brainstormed words
+
+# How it works?
+
+Just put your ideas for domains in "list_of_words.txt" and run ```npm start```
+
+# Config
+
+Use the ```.nic.io-scannerrc``` to configure the app. Please, keep in mind that tweaking the ```minimumSeconds``` and ```maximumSeconds``` affect the time, that you execute a query to https://nic.io, and if you put too short interval, they will block your access to the site.
---
+++ io-scanner.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const appName = require('./package.json').name
+const config = require('rc')(appName)
+const fs = require('fs')
+const request = require('request')
+const cheerio = require('cheerio')
+const LineByLineReader = require('line-by-line')
+const lr = new LineByLineReader(config.wordsFile)
+const headers = {
+    'User-Agent':       'j8 Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded'
+}
+const logger = require('bunyan').createLogger({name: 'node.io-scanner', level: process.env['BUNYAN_LEVEL'] || 'info'})
+
+let minSec = config.minimumSeconds
+let maxSec = config.maximumSeconds
+let words = 0
+
+function randomInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+lr.on('error', function (err) {
+    logger.error(`==== Error scanning ${err} ====`);
+})
+
+lr.on('line', function (word) {
+    lr.pause()
+    words += 1
+	var word = word
+
+    setTimeout(function () {
+    	request({url: config.url, method: 'GET', headers: headers, qs: {'query': word + config.extension}, timeout: config.requestTimeout}, function(error, response, html){
+    		if(!error && response.statusCode == 200){
+    			var $ = cheerio.load(html)
+    			logger.info(`====  ${words}) Checking domain: ${word}${config.extension} ====`);
+
+    	        $("#bodyfill b").filter(function() {
+    	        	 if( $(this).text().indexOf('pendingDelete') >= 0) {
+    		        	logger.warn(`==== Domain ${word}${config.extension} pending delete ${$(this).text()} ====`);
+
+    	        	 	lr.resume()
+    	        	 } else if ( $(this).text().indexOf('This Domain is') >= 0) {
+    	        	 	logger.warn(`==== BAZINGA! Domain ${word}${config.extension} is free ====`);
+    	        	 	lr.resume()
+    	        	 } else {
+    	        	 	lr.resume()
+    	        	 }
+    	        })
+    		} else {
+    			logger.error(`==== Error or timeout ====`);
+    			lr.resume()
+    		}
+    	})
+
+    }, randomInterval(minSec,maxSec))
+})
